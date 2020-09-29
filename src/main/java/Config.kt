4f5d78d495e94c7409fa5c3ec2efe0c8f9144e50
Florian Schmidt <florian.schmidt@platis.de>
2020-09-29 08:26:56 +0200

    Moved to Kotlin 1.4

--- src/main/java/Config.kt
+++ src/main/java/Config.kt
@@ -3,77 +3,77 @@ object Config {
     object Module {
         object Template {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-module-template"
         }
         object MathcatBundle {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-bundle"
         }
         object MathcatContinuation {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-continuation"
         }
         object MathcatCore {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-core"
         }
         object MathcatLens {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-lense"
         }
         object MathcatMorphism {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-morphism"
         }
         object MathcatPrism {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-prism"
         }
         object MathcatProfunctorOptics {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-profunctor-optics"
         }
         object MathcatReader {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-reader"
         }
         object MathcatResult {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-result"
         }
         object MathcatState {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-state"
         }
         object MathcatStore {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-store"
         }
         object MathcatStructure {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-structure"
         }
         object MathcatYoneda {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-yoneda"
         }
         object MathcatOperatorTree {
             val group = "org.evoleq"
-            val version = "1.0.0"
+            val version = "1.0.1"
             val artefactId = "mathcat-operator-tree"
         }
         
@@ -81,8 +81,8 @@ object Config {
     
     object Versions {
 
-        val kotlin = "1.3.70"
-        val coroutines = "1.3.5"
+        val kotlin = "1.4.0"//"1.3.70"
+        val coroutines = "1.3.9"//"1.3.5"
      //   val kotlinReflect = "1.3.70"
      //   val kotlinSerialization = "0.20.0"//""0.14.0"
         val junit = "4.12"
